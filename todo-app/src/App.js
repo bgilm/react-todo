c8bc6cee31602e1c63f59cd4a5549183b22d0b6c
bgilm <benjamingil94@gmail.com>
2018-03-05 16:47:25 -0600

    Add more functionality

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import NewTodo from './components/NewTodo'
 import TodoList from './components/TodoList'
+import Footer from './components/Footer'
 import { guid } from './utils/uuid'
+import { TodoContainer } from './App.style'
+import MultiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   constructor (props) {
@@ -18,6 +21,11 @@ class App extends Component {
     this.addTodo = this.addTodo.bind(this)
     this.deleteTodo = this.deleteTodo.bind(this)
     this.toggleIsDone = this.toggleIsDone.bind(this)
+    this.checkEverything = this.checkEverything.bind(this)
+    this.getTodos = this.getTodos.bind(this)
+    this.clearCompleted = this.clearCompleted.bind(this)
+    this.renderFooter = this.renderFooter.bind(this)
+    this.setVisibilityFilter = this.setVisibilityFilter.bind(this)
   }
 
   addTodo(value) {
@@ -53,19 +61,56 @@ class App extends Component {
     const todos = actualTodos.filter(todo => todo.isDone === false )
     this.setState({todos})
   }
+  renderFooter() {
+    if(this.state.todos.length > 0){
+      return (
+        <Footer
+        itemsLeft={this.state.todos.filter(todo => todo.isDone === false).length}
+        clearCompleted={this.clearCompleted}
+        setVisibilityFilter={this.setVisibilityFilter}
+        visibilityFilter={this.state.visibilityFilter}
+        />)
+    }
+    return null
+  }
 
+  getTodos(filter) {
+    const todos = this.state.todos
+    switch (filter) {
+      case 'All':
+        return todos
+      case 'Active':
+        return todos.filter(todo => todo.isDone === false)
+      case 'Completed':
+        return todos.filter(todo => todo.isDone === true)
+      default:
+      break;
+    }
+  }
 
+  setVisibilityFilter(visibilityFilter) {
+    this.setState({visibilityFilter})
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React todo app</h1>
-        </header>
-        <NewTodo addTodo={this.addTodo} />
-        <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo} toggleIsDone={this.toggleIsDone}/>
-      </div>
+      <MultiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">React todo app</h1>
+          </header>
+          <TodoContainer>
+            <NewTodo checkEverything={this.checkEverything} isEverythingChecked={this.state.isEverythingChecked} addTodo={this.addTodo} />
+            <TodoList
+              todos={this.getTodos(this.state.visibilityFilter)}
+              deleteTodo={this.deleteTodo}
+              toggleIsDone={this.toggleIsDone}
+            />
+            {this.renderFooter()}
+          </TodoContainer>
+        </div>
+      </MultiThemeProvider>
     );
   }
 }
