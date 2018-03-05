c98e3d82b0c5a8810428fe4b8b683715f1787b6e
bgilm <benjamingil94@gmail.com>
2018-03-05 12:26:52 -0600

    Add main functionality and state design

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,24 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NewTodo from './components/NewTodo'
+import TodoList from './components/TodoList'
+import { guid } from './utils/uuid'
 
 class App extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      todos:[]
+      todos:[],
+      isEverythingChecked: false,
+      visibilityFilter: 'All'
     }
 
     this.addTodo = this.addTodo.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.toggleIsDone = this.toggleIsDone.bind(this)
   }
 
   addTodo(value) {
-    const todos = [...this.state.todos, { value, isDone: false }]
-    this.setState({
-      todos
-    })
+    const todos = [...this.state.todos, { id: guid(), value, isDone: false }]
+    this.setState({ todos })
   }
+
+  deleteTodo(id) {
+    const actualTodos = this.state.todos
+    const todos = actualTodos.filter(todo => todo.id !== id)
+    this.setState({ todos })
+  }
+
+  toggleIsDone(id) {
+    let todos = this.state.todos
+    const foundIndex = todos.findIndex(todo => todo.id === id);
+    todos[foundIndex].isDone = !todos[foundIndex].isDone
+    this.setState({ todos })
+  }
+
+  clearEverything() {
+    this.setState({todos:[]})
+  }
+
+  checkEverything() {
+    const { todos, isEverythingChecked } = this.state
+    todos.forEach(todo => todo.isDone = !isEverythingChecked)
+    this.setState({ todos, isEverythingChecked: !isEverythingChecked })
+  }
+
+  clearCompleted() {
+    const actualTodos = this.state.todos
+    const todos = actualTodos.filter(todo => todo.isDone === false )
+    this.setState({todos})
+  }
+
+
 
   render() {
     return (
@@ -29,6 +64,7 @@ class App extends Component {
           <h1 className="App-title">React todo app</h1>
         </header>
         <NewTodo addTodo={this.addTodo} />
+        <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo} toggleIsDone={this.toggleIsDone}/>
       </div>
     );
   }
